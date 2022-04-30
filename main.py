8fc68b799f97dde4e6f8cf8da6ef07b212f50e30
Josh Mann <jlmann72@gmail.com>
2022-04-30 06:44:13 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from tkinter import *
+# from tkinter import ttk
+# from tkinter import messagebox
+window = Tk()
+
+window.title('Hello Python')
+window.geometry('300x200+10+20')
+window.mainloop()
+
+
+def main():
+    pass
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
