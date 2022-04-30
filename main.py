98723ad5bc6ff47baeec1a12b93742a8c70d610a
Josh Mann <jlmann72@gmail.com>
2022-04-30 09:14:32 -0400

    Added a label

--- main.py
+++ main.py
@@ -3,20 +3,20 @@ from tkinter import *
 # from tkinter import messagebox
 window = Tk()
 
-btn = Button(window, text='This is a Button widget', fg='blue')
-btn.place(x=80, y=100)
 
-lbl = Label(window, text='This is a Label widget', fg='red', font=('Helvetica', 16))
-lbl.place(x=60, y=50)
+def button1_clicked():
+    label1.config(text='Clicked!!')
+
+
+button1 = Button(window, text='Click Me!', fg='blue', command=button1_clicked)
+button1.place(x=80, y=100)
+
+label1 = Label(window, text='This is a Label widget', fg='red', font=('Helvetica', 16))
+label1.place(x=60, y=50)
+
+text_field = Entry(window, fg='yellow', bd=5)
+text_field.place(x=80, y=150)
+
 window.title('Hello Python')
 window.geometry('300x200+10+20')
 window.mainloop()
-
-
-# def main():
-#     pass
-#
-#
-# # Press the green button in the gutter to run the script.
-# if __name__ == '__main__':
-#     main()
