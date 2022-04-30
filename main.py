deff8645974ae212288fe50ff86038818055dc49
Josh Mann <jlmann72@gmail.com>
2022-04-30 06:52:35 -0400

    Added a button

--- main.py
+++ main.py
@@ -3,15 +3,18 @@ from tkinter import *
 # from tkinter import messagebox
 window = Tk()
 
+btn = Button(window, text='This is a Button widget', fg='blue')
+btn.place(x=80, y=100)
+
 window.title('Hello Python')
 window.geometry('300x200+10+20')
 window.mainloop()
 
 
-def main():
-    pass
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    main()
+# def main():
+#     pass
+#
+#
+# # Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     main()
