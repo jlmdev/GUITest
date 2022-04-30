7598f4edd532093571db5e5af1ba123a1470fd0b
Josh Mann <jlmann72@gmail.com>
2022-04-30 07:08:04 -0400

    Added a label

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ window = Tk()
 btn = Button(window, text='This is a Button widget', fg='blue')
 btn.place(x=80, y=100)
 
+lbl = Label(window, text='This is a Label widget', fg='red', font=('Helvetica', 16))
+lbl.place(x=60, y=50)
 window.title('Hello Python')
 window.geometry('300x200+10+20')
 window.mainloop()
